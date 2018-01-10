de394ac8bef10704f630e155eb76b17b418a78ab
st1hy <st1hy@outlook.com>
2018-01-10 20:36:25 +0100

    Initial commit

---
+++ src/com/github/st1hy/simplecalculator/Controller.java
@@ -0,0 +1,4 @@
+package com.github.st1hy.simplecalculator;
+
+public class Controller {
+}
---
+++ src/com/github/st1hy/simplecalculator/MainApp.java
@@ -0,0 +1,28 @@
+package com.github.st1hy.simplecalculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ResourceBundle strings = ResourceBundle.getBundle("strings");
+        URL main = getClass().getResource("main.fxml");
+        Parent root = FXMLLoader.load(main, strings);
+        primaryStage.setTitle(strings.getString("window.title"));
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
